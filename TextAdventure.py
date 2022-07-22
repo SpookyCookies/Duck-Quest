06ed6027868d914d40875ba15f840a7070328cd6
SpookyCookies <87934844+SpookyCookies@users.noreply.github.com>
2022-07-22 02:27:48 -0500

    Add files via upload

--- TextAdventure.py
+++ TextAdventure.py
@@ -9,75 +9,75 @@ def FirstUserChoice():
             print('The room is dark, almost no light however, you are still able to see that there is a door')
             FirstUserChoice()
         if UserChoiceOne == 'Push':
-            os.system('cls')
+            os.system('clear')
             print('What are you pushing on?')
             FirstUserChoice()
         if UserChoiceOne == 'Open':
-            os.system('cls')
+            os.system('clear')
             print('What are you opening?')
             FirstUserChoice()
         if UserChoiceOne == 'Open Door':
-            os.system('cls')
+            os.system('clear')
             typeanimation(FancyRoom)
             SecondUserChoice()
         if UserChoiceOne == 'Push Door':
-            os.system('cls')
+            os.system('clear')
             print('How embarrasing!, this door is pull door')
             FirstUserChoice()
         if UserChoiceOne == 'Push Wall':
-            os.system('cls')
+            os.system('clear')
             print('the fuck are you doing, stop that')
             FirstUserChoice()
         if UserChoiceOne == 'Push Candle':
-            os.system('cls')
+            os.system('clear')
             typeanimation(CandelEnding)
             quit()
         if UserChoiceOne == 'Open Candle':
-            os.system('cls')
+            os.system('clear')
             print('How would you open a candle...')
             time.sleep(5)
             print('Nevermind')
             FirstUserChoice()
         if UserChoiceOne == "Describe Room":
-            os.system('cls')
+            os.system('clear')
             print('The room is dark, almost no light however, you are still able to see that there is a door')
             FirstUserChoice()
         if UserChoiceOne == "Describe Candle":
-            os.system('cls')
+            os.system('clear')
             print('Old Fashioned Candle, not safe to eat but safe to look at')
             FirstUserChoice()
         if UserChoiceOne == "Describe Door":
-            os.system('cls')
+            os.system('clear')
             print('Very beaten down wooden door looks looks like its been around for a while')
             FirstUserChoice()
         if UserChoiceOne == "Describe Me":
-            os.system('cls')
+            os.system('clear')
             print('Beautiful')
             FirstUserChoice()
         else:
-            os.system('cls')
+            os.system('clear')
             print("You canot",UserChoiceOne,"Remember to interact with the world you must use Describe/Push/Open/ and in some Special cases With")
             FirstUserChoice()
 def SecondUserChoice():
                 UserChoiceTwo = input('What are you going to do now:  ')
                 if UserChoiceTwo == 'Push':
-                    os.system('cls')
+                    os.system('clear')
                     print('What are you pushing on?')
                     SecondUserChoice()
                 if UserChoiceTwo == 'Open':
-                    os.system('cls')
+                    os.system('clear')
                     print('What are you opening?')
                     SecondUserChoice()
                 if UserChoiceTwo == 'Describe':
-                    os.system('cls')
+                    os.system('clear')
                     print('The room is fancy with many \u001b[33mexpensive light fixtures\u001b[0m a piano and a table with some hot coca on it and a lot of Cummus Lord FunkoPops however there seems to be a hatch on the floor, interesting')
                     SecondUserChoice()
                 if UserChoiceTwo == 'Describe Room':
-                    os.system('cls')
+                    os.system('clear')
                     print('The room is fancy with many \u001b[33mexpensive light fixtures\u001b[0m a piano and a table with some hot coca on it  and a lot of Cummus Lord FunkoPops however there seems to be a hatch on the floor, interesting')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe FunkoPops":
-                    os.system('cls')
+                    os.system('clear')
                     print('White box with plastic lining the sides letting you see the LIMITED EDITION Cummus Lord FunkoPops')
                     time.sleep(3)
                     print('This ones been used...')
@@ -85,177 +85,177 @@ def SecondUserChoice():
                     print('ew')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Hatch":
-                    os.system('cls')
+                    os.system('clear')
                     print('Decently sized hatch,\u001b[31m perfect to fit you.\u001b[0m')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Expensive Light Fixtures":
-                    os.system('cls')
+                    os.system('clear')
                     print('Look expensive, how did they get here anyway?')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Table":
-                    os.system('cls')
+                    os.system('clear')
                     print('Its a nice table\u001b[31m would be a shame if something happened to it...\u001b[0m')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Piano":
-                    os.system('cls')
+                    os.system('clear')
                     print('Old fashion piano looks expensive like everything else does here')
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Hidden Door":
-                    os.system('cls')
+                    os.system('clear')
                     print('Its a hidden door, originally hidden by FunkoPops,\u001b[31mI wonder what would happen if you open it...\u001b[0m')    
                     SecondUserChoice()
                 if UserChoiceTwo == "Describe Hot Coca":
-                    os.system('cls')
+                    os.system('clear')
                     print('\u001b[31mHot\u001b[0m to the touch \u001b[31mHot\u001b[0m to drink')
                     SecondUserChoice()
                 if UserChoiceTwo == "Push Coca":
-                    os.system('cls')
+                    os.system('clear')
                     print('\u001b[31mWhy?\u001b[0m')
                     SecondUserChoice()
                 if UserChoiceTwo == "Push Expensive Light Fixtures":
-                    os.system('cls')
+                    os.system('clear')
                     print('\u001b[31mYou broke them, you ungrateful fuck, STOP BREAKING SHIT\u001b[0m')
                     SecondUserChoice()
                 if UserChoiceTwo == "Open Coca":
-                     os.system('cls')
+                     os.system('clear')
                      print('How would you open Coca?')
                      SecondUserChoice()
                 if UserChoiceTwo == 'Open FunkoPops':
-                    os.system('cls')
+                    os.system('clear')
                     print('Better not they look expensive and in mint condition too...')
                     SecondUserChoice()
                 if UserChoiceTwo == "Open Table":
-                     os.system('cls')
+                     os.system('clear')
                      print('Why would you think this would work?')
                      SecondUserChoice()
                 if UserChoiceTwo == "Open Piano":
-                     os.system('cls')
+                     os.system('clear')
                      print('And how are you going to do that smart guy?')
                      SecondUserChoice()
                 if UserChoiceTwo == "Push Piano":
-                     os.system('cls')
+                     os.system('clear')
                      print('No pushing the piano')
                      SecondUserChoice()
                 if UserChoiceTwo == "Play Piano":
-                    os.system('cls')
+                    os.system('clear')
                     print('You start playing the most beautiful song known to man, its amazing')
                     SecondUserChoice()     
                 if UserChoiceTwo == 'Open Hatch':
-                    os.system('cls')
+                    os.system('clear')
                     typeanimation(Outside)
                     LastUserChoice()
                 if UserChoiceTwo == 'Push Hatch':
-                    os.system('cls')
+                    os.system('clear')
                     print('Open it, lol')
                     SecondUserChoice()
                 if UserChoiceTwo == 'Push Table':
-                    os.system('cls')
+                    os.system('clear')
                     typeanimation2(BreakTable)
                     SecondUserChoice()
                 if UserChoiceTwo == 'Open Hidden Door':
-                    os.system('cls')
+                    os.system('clear')
                     typeanimation(SpaceEnding)
                     quit()
                 if UserChoiceTwo == 'Push FunkoPops':
-                    os.system('cls')
+                    os.system('clear')
                     print('You pushed over the funkopops and now they are all over the floor, good going asshole, however you notice that there is a door')
                     SecondUserChoice()
                 else:
-                    os.system('cls')
+                    os.system('clear')
                     print("Again, You canot",UserChoiceTwo," please only use Describe/Push/Open/ and in some Special cases With")
                     SecondUserChoice()
 def LastUserChoice():
                 UserChoiceLast = input('What are you going to do now:  ')
                 if UserChoiceLast == 'Push':
-                    os.system('cls')
+                    os.system('clear')
                     print('I told you again and again and again\u001b[31m WHAT ARE YOU PUSHING????\u001b[0m')
                     LastUserChoice()
                 if UserChoiceLast == 'Open':
-                    os.system('cls')
+                    os.system('clear')
                     print('ughhh... what are you opening?')
                     LastUserChoice()
                 if UserChoiceLast == "Describe":
-                    os.system('cls')
+                    os.system('clear')
                     print('You seem to be on the side of a road with a car standing in front of you, its freezing cold out and snow is everywhere')
                     LastUserChoice()
                     LastUserChoice()
                 if UserChoiceLast == "Describe Outside":
-                    os.system('cls')
+                    os.system('clear')
                     print('You seem to be on the side of a road with a car standing in front of you, its freezing cold out and snow is everywhere')
                     LastUserChoice()
                 if UserChoiceLast == "Describe Trunk":
-                     os.system('cls')
+                     os.system('clear')
                      print('A trunk \u001b[31m perfect for storing dead bodies...\u001b[0m just messing around, I wonder whats inside of it')
                      LastUserChoice()
                 if UserChoiceLast == "Describe Road":
-                    os.system('cls')
-                    print('Covered with snow and ice, \u001b[31m almost seems to be fake\u001b[0m')
+                    os.system('clear')
+                    print('Covered with snow and ice')
                     LastUserChoice()
                 if UserChoiceLast == "Describe Car Door":
-                    os.system('cls')
+                    os.system('clear')
                     print('Just a car door')
                     LastUserChoice()
                 if UserChoiceLast == "Describe Snow":
-                    os.system('cls')
-                    print('White stuff that falls from the sky')
+                    os.system('clear')
+                    print('White stuff that falls from the sky,  \u001b[31m almost seems to be fake\u001b[0m')
                     LastUserChoice()
                 if UserChoiceLast == "Describe Key":
-                    os.system('cls')
+                    os.system('clear')
                     print('Looks like the key to a car')
                     LastUserChoice()
                 if UserChoiceLast == "Open Snow":
-                    os.system('cls')
+                    os.system('clear')
                     print('How would you possibly think to open snow?')
                     LastUserChoice()
                 if UserChoiceLast == "Open Road":
-                    os.system('cls')
+                    os.system('clear')
                     print('Some call it the open road for a reason I guess...')
                     LastUserChoice()
                 if UserChoiceLast == "Open Key":
-                     os.system('cls')
+                     os.system('clear')
                      print('Why in your right mind would you think to open a key?')
                      time.sleep(3)
                      print('I swear you got a few nuts & bolts loose upstairs dont you?')
                      LastUserChoice()
                 if UserChoiceLast == "Push Car Door":
-                    os.system('cls')
+                    os.system('clear')
                     print('Dont push it you might break it')
                     LastUserChoice()
                 if UserChoiceLast == "Push Key":
-                    os.system('cls')
+                    os.system('clear')
                     print('Might want to try Open Car Door With Key')
                     LastUserChoice()
                 if UserChoiceLast == "Push Road":
-                    os.system('cls')
+                    os.system('clear')
                     print('Its as strudy as it will ever be')
                     LastUserChoice()
                 if UserChoiceLast == "Push Me":
-                     os.system('cls')
+                     os.system('clear')
                      print('You are actually really pushing my buttons')
                      LastUserChoice()
 
                 if UserChoiceLast == "Open Trunk":
-                    os.system('cls')
+                    os.system('clear')
                     print('You open the trunk with ease and you see a key, you pick it up')
                     LastUserChoice()
                 if UserChoiceLast == 'Open Door':
-                    os.system('cls')
+                    os.system('clear')
                     print('You may want to try Open Car Door')
                     LastUserChoice()
                 if UserChoiceLast == "Open Car Door":
-                    os.system('cls')
+                    os.system('clear')
                     print('\u001b[31mIts locked\u001b[0m you may want to try the trunk of the car' )
                     LastUserChoice()
                 if UserChoiceLast == 'Open Car Door With Key':
-                    os.system('cls')
+                    os.system('clear')
                     typeanimation(CarEnding)
                     quit()
                 if UserChoiceLast == "Push Snow":
-                    os.system('cls')
+                    os.system('clear')
                     typeanimation(RealEnding)
                     quit()
                 else:
-                    os.system('cls')
+                    os.system('clear')
                     print('You Canot',UserChoiceLast,'Please Remember To only use Describe/Push/Open/ and in some Special cases With')
                     LastUserChoice()
 IntroText = "\u001b[32mWelcome to: A Ducks Quest Of Time!\n\
@@ -292,7 +292,7 @@ def typeanimation(IntroText):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.1)
-os.system('cls')
+os.system('clear')
 
 typeanimation(IntroText)
 def typeanimation2(IntroText):
@@ -300,7 +300,7 @@ def typeanimation2(IntroText):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.4)
-os.system('cls')
+os.system('clear')
 def StartOfGame():
     UserInputOne = input()
     if UserInputOne == "Y":
